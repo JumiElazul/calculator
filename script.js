0396505fb2d174700ee0a1bc3ee5b720c40e1876
Gregory Coomber <gr.coomber@gmail.com>
2021-01-11 12:27:10 -0800

    Added event listeners to all buttons, with forEach loops on NodeList types.

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 const numberInputs = document.querySelectorAll('.input-number')
 const operatorInputs = document.querySelectorAll('.input-operator')
 
+const period = document.querySelector('.input-period')
 const backspace = document.querySelector('.input-clear')
 const clearAll = document.querySelector('.input-clear-all')
-const period = document.querySelector('.input-period')
+const enterKey = document.querySelector('.input-enter-key')
 const inputArea = document.querySelector('.current-input')
 const outputArea = document.querySelector('.output')
+
 
 // List of all legal operations.
 
@@ -32,3 +34,31 @@ function divide (num1, num2) {
 }
 
 // Adding event listeners to all keys on the calculator.
+
+numberInputs.forEach (function (button) {
+	button.addEventListener('click', //)
+})
+
+operatorInputs.forEach (function (button) {
+	button.addEventListener('click', //)
+})
+
+period.addEventListener('click', () =>
+{
+	console.log('period')
+})
+
+backspace.addEventListener('click', () =>
+{
+	console.log('backspace')
+})
+
+clearAll.addEventListener('click', () =>
+{
+	console.log('clear all')
+})
+
+enterKey.addEventListener('click', () => 
+{
+	console.log('enterkey')
+})
