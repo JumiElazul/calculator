bd9d9f348d6a0d1f58d37321e83b0297e10946af
Gregory Coomber <gr.coomber@gmail.com>
2021-01-11 10:00:42 -0800

    Basic calculator layout complete.  Basics of keyboard input function added to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Creating function and event listener to take keyboard input from the window.
+
+// function keyboardPress (e)
+// {
+// 	const validKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+', 'Enter']
+
+// 	if (validKeys.includes(e.key))
+// 	{
+// 		keyDown = e.key
+// 	}
+// }
+
+// window.addEventListener('keydown', keyboardPress)
