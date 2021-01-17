a1c9d1c4247db7f01e23fa20aa4d44a3a20193d6
Gregory Coomber <gr.coomber@gmail.com>
2021-01-17 15:05:13 -0800

    Animations added for all keyboard presses except mod

--- script.js
+++ script.js
@@ -34,37 +34,113 @@ function keyboardEventHandler (e)
 		
 		if (!isNaN(key))
 		{
+
+			numberInputs.forEach (function (button) {
+				if (button.textContent == key)
+				{
+					button.classList.remove('animate-number-button-class')
+
+					setTimeout (function () {
+						button.classList.add('animate-number-button-class')
+					}, 5)
+				}
+			})
+
 			appendNumber(key)
 		}
 		else
 		{
+			let oper = null
 			switch (key)
 			{
 				case '+':
+					oper = document.getElementById('plus-but')
+
+					oper.classList.remove('animate-operator-button-class')
+
+					setTimeout (function () {
+						oper.classList.add('animate-operator-button-class')
+					}, 5)
 					appendOperator('+')
 					break
+
 				case '-':
+					oper = document.getElementById('minus-but')
+
+					oper.classList.remove('animate-operator-button-class')
+
+					setTimeout (function () {
+						oper.classList.add('animate-operator-button-class')
+					}, 5)
 					appendOperator('-')
 					break
+
 				case '*':
+					oper = document.getElementById('star-but')
+
+					oper.classList.remove('animate-operator-button-class')
+
+					setTimeout (function () {
+						oper.classList.add('animate-operator-button-class')
+					}, 5)
 					appendOperator('*')
 					break
+
 				case '/':
+					oper = document.getElementById('divide-but')
+
+					oper.classList.remove('animate-operator-button-class')
+
+					setTimeout (function () {
+						oper.classList.add('animate-operator-button-class')
+					}, 5)
 					appendOperator('/')
 					break
+
 				case '%':
+					oper = document.getElementById('mod-but')
+
+					oper.classList.remove('animate-operator-button-class')
+
+					setTimeout (function () {
+						oper.classList.add('animate-operator-button-class')
+					}, 5)
 					appendOperator('%')
 					break
+
 				case '.':
+					periodKey.classList.remove('animate-number-button-class')
+
+					setTimeout (function () {
+						periodKey.classList.add('animate-number-button-class')
+					}, 5)
 					appendPeriod(workingCalculation)
 					break
+
 				case 'Enter':
+					enterKey.classList.remove('animate-enter-button-class')
+
+					setTimeout (function () {
+						enterKey.classList.add('animate-enter-button-class')
+					}, 5)
 					operate(workingCalculation, currentOperator)
 					break
+
 				case 'Backspace':
+					backspaceKey.classList.remove('animate-clear-button-class')
+
+					setTimeout (function () {
+						backspaceKey.classList.add('animate-clear-button-class')
+					}, 5)
 					backspace(workingCalculation)
 					break
+
 				case 'Escape':
+					clearAllKey.classList.remove('animate-clear-button-class')
+
+					setTimeout (function () {
+						clearAllKey.classList.add('animate-clear-button-class')
+					}, 5)
 					clearAll()
 			}
 		}
@@ -338,6 +414,14 @@ numberInputs.forEach (function (button) {
 			button.classList.add('animate-number-button-class')
 		}, 5)
 	})
+})
+
+periodKey.addEventListener('click', () => {
+	periodKey.classList.remove('animate-number-button-class')
+
+	setTimeout(function () {
+		periodKey.classList.add('animate-number-button-class')
+	}, 5)
 })
 
 operatorInputs.forEach (function (button) {
